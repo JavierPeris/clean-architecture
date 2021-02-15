3d86f5e59a327ef59a5f2d28d14c501174228c27
Javier Peris <javierperis@gmail.com>
2021-02-15 16:58:33 +0100

    Initialize service

---
+++ src/test/java/org/javierperis/pricing/PricingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javierperis.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
