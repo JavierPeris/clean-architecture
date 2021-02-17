7596d4f0ed73ea9af920d674b5434c8b2cb54981
Javier Peris <javierperis@gmail.com>
2021-02-17 10:51:55 +0100

    Add minimum test for the JPA adapter

---
+++ src/main/java/org/javierperis/pricing/JpaPrice.java
@@ -0,0 +1,12 @@
+package org.javierperis.pricing;
+
+import java.util.List;
+
+public class JpaPrice implements PriceCalculationDsGateway {
+
+    @Override
+    public List<PriceDsResponseModel> getPrices(PriceDsRequestModel priceDsRequestModel) {
+        return null;
+    }
+
+}
---
+++ src/test/java/org/javierperis/pricing/JpaPriceTest.java
@@ -0,0 +1,25 @@
+package org.javierperis.pricing;
+
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Transactional
+public class JpaPriceTest {
+
+    private static final Long ZARA_BRAND_ID = 1L;
+    private static final Long PRODUCT_ID = 35455L;
+    private static final LocalDateTime date = LocalDate.of(2020, 6, 14)
+            .atTime(0, 0);
+    private final JpaPrice jpaPrice = new JpaPrice();
+
+    @Test
+    public void givenNoPrices_whenRequest_ThenReturnEmptyList() {
+        PriceDsRequestModel priceDsRequestModel = new PriceDsRequestModel(ZARA_BRAND_ID, PRODUCT_ID, date);
+        jpaPrice.getPrices(priceDsRequestModel);
+    }
+
+
+}
