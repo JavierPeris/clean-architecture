0f99601cf14fe8f9dcb7c980fc9c92becebb97f6
Javier Peris <javierperis@gmail.com>
2021-02-17 10:11:40 +0100

    Add test to check the fail view for the implemented price presenter

---
+++ src/test/java/org/javierperis/pricing/PriceResponseFormatterTest.java
@@ -0,0 +1,19 @@
+package org.javierperis.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PriceResponseFormatterTest {
+
+    PriceResponseFormatter priceResponseFormatter = new PriceResponseFormatter();
+
+    @Test
+    void whenPrepareFailView_thenThrowHttpConflictException() {
+        assertThatThrownBy(() -> priceResponseFormatter
+                .prepareFailView("There's no price for the product and date specified"))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+
+}
